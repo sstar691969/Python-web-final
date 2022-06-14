b7cd3a8df382443c63506d65a03d30ffe23b3104
Grimwood73 <jonah.byrne@gmail.com>
2022-06-13 23:12:10 -0700

    New Movie form commit

---
+++ bluemouseapp/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+from .models import Movie, Ticket, Customer
+
+class MovieForm(forms.ModelForm):
+    class Meta:
+        model=Movie
+        fields='__all__'
+
+class TicketForm(forms.ModelForm):
+    class Meta:
+        model=Ticket
+        fields='__all__'
+
+class CustomerForm(forms.ModelForm):
+    class Meta:
+        model=Customer
+        fields='__all__'
--- bluemouseapp/views.py
+++ bluemouseapp/views.py
@@ -1,5 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Movie, Customer, Showing, Ticket
+from django.urls import reverse_lazy
+from .forms import MovieForm
 
 # Create your views here.
 def index(request):
   return render(request, 'bluemouseapp/index.html')
+
+def newMovie(request):
+  form=MovieForm
+
+  if request.method=='POST':
+    form=MovieForm(request.POST)
+    if form.is_valid():
+      post=form.save(commit=True)
+      post.save()
+      form=MovieForm()
+  else:
+    form=MovieForm()
+  return render(request, 'bluemouseapp/newmovie.html', {'form': form})
