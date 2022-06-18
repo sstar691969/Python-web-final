dc65b2818b7cedd2816cf0bc5c18a677478ecc94
Nhi <thaonhi.nguyen411@gmail.com>
2022-06-17 18:41:06 -0700

    view and view details commit

--- bluemouseapp/urls.py
+++ bluemouseapp/urls.py
@@ -4,6 +4,12 @@ from . import views
 
 urlpatterns = [
    path('',views.index, name='index'),
+   path('movies/', views.movies, name='movies'),
+   path('moviedetail/<int:id>', views.moviedetail, name='moviedetail'),
+   path('showings/', views.showings, name='showings'),
+   path('showingdetail/<int:id>', views.showingdetail, name='showingdetail'),
+   path('customers/', views.customers, name='customers'),
+   path('customerdetail/<int:id>', views.customerdetail, name='customerdetail'),
    path('newmovie/', views.newMovie, name='newmovie'),
    path('newcustomer/', views.newCustomer, name='newcustomer'),
    path('newshowing/', views.newShowing, name='newshowing'),
